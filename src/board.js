c0d0f509705d71602c2e8be3f3ff28e6fd485936
Filip Kulig <filipkulig@gmail.com>
2016-07-02 01:04:57 +0100

    Proper building and buildings classes.

--- src/board.js
+++ src/board.js
@@ -1,5 +1,8 @@
 function Board() {
 
+    this.x = 10;
+    this.y = 10;
+
     if ( ! this instanceof Board )
         return new Board();
 
@@ -12,6 +15,14 @@ Board.prototype.draw = function () {
 }
 
 Board.prototype.nameBox = function (name) {
+
     var nameBox = _.template('<p class="name_box"> Player name: <b><%=  name %></b></p>');
     $("#name").html(nameBox({name: name}))
+
 }
+
+
+Board.prototype.drawCity = function () {
+    
+}
+
--- src/buildings.js
+++ src/buildings.js
@@ -1,34 +1,16 @@
 function Building() {
 
-    var name = null;
-
-    var setName = function(_name) {
-        name  = _name;
-    }
-
-    return {
-        'setName': function (_name){ setName(_name)}
-    }
+    if ( ! this instanceof Building)
+        return new Building();
 
 }
-
 
 function Buildings() {
 
+    if ( ! this instanceof Buildings)
+        return new Buildings();
 
-    this.buildings = [];
+    this.buildings = []
 
-    this.add = function(name)
-    {
-        this.building = new Building()
-        this.building.setName("House")
-        this.buildings.push(building)
-    }
-
-    return {
-
-        //'buildings' : buildings,
-        //'add' : function(name) {add(name)}
-
-    }
 }
+
